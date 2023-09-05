c4ef197c6d25e13357ac3a7d9b9e12907dfccfba
Alex Ford <Alex@CatDadCode.dev>
2023-09-04 18:48:51 -0600

    Working install script

--- install.sh
+++ install.sh
@@ -3,18 +3,53 @@
 # Get absolute path of current script.
 DOTFILES_DIR=$(pwd)
 
+# Upload 
+GITHUB_USERNAME=CatDadCode
+SSH_KEY=$HOME/.ssh/id_rsa
+
+if [ ! -f $SSH_KEY ] || [ ! -f $SSH_KEY.pub ]; then
+  echo 'Generate your ssh key first!'
+  exit
+fi
+
+github_result=0
+echo 'How do you want to name the key?'
+read -e KEY_NAME
+until [ $github_result -eq 1 ]; do
+  curl --silent -u "$GITHUB_USERNAME" --data "{\"title\":\"$KEY_NAME\",\"key\":\"$(cat $SSH_KEY.pub)\"}" https://api.github.com/user/keys > gh-result
+  if grep -q 'key is already in use' gh-result; then
+    github_result=1
+    echo 'Key is already in use.'
+  elif grep -q '"verified": true' gh-result; then
+    github_result=1
+    echo "Key ($KEY_NAME) has been added successfully."
+  elif grep -q 'Bad credentials' gh-result; then
+    echo 'Error during login: invalid username or password!'
+    echo 'Try again!'
+  else
+    echo 'A problem occured during the upload!'
+    cat gh-result
+    echo 'Try again!'
+  fi
+  rm gh-result
+done
+
+# Install prerequisites.
+# TODO: Adjust this to not run on OSX.
+sudo apt install build-essential -y
+
 # Ensure directories exist.
 mkdir -p "$HOME/.config/htop"
 mkdir -p "$HOME/.nvm"
 
 # Remove any existing files.
-rm "$HOME/.config/htop/htoprc"
-rm -r "$HOME/.config/nvim"
-rm -r "$HOME/.oh-my-zsh"
-rm "$HOME/.gitconfig"
-rm "$HOME/.p10k.zsh"
-rm "$HOME/.zprofile"
-rm "$HOME/.zshrc"
+rm -f "$HOME/.config/htop/htoprc"
+rm -rf "$HOME/.config/nvim"
+rm -rf "$HOME/.oh-my-zsh"
+rm -f "$HOME/.gitconfig"
+rm -f "$HOME/.p10k.zsh"
+rm -f "$HOME/.zprofile"
+rm -f "$HOME/.zshrc"
 
 # Create symlinks.
 ln -s "$DOTFILES_DIR/.config/htop/htoprc" "$HOME/.config/htop/htoprc"
@@ -25,7 +60,7 @@ ln -s "$DOTFILES_DIR/.zprofile" "$HOME/.zprofile"
 ln -s "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
 
 # Install Homebrew.
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # Update Homebrew
@@ -35,18 +70,15 @@ brew update && brew upgrade
 brew install zsh
 
 # Install remaining packages.
-brew install build-essential gcc fd neovim nvm ripgrep zsh 
+brew install gcc fd neovim nvm ripgrep zsh 
 
 # Install Oh My ZSH!
-RUNZSH=no
-KEEP_ZSHRC=yes
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-mkdir "$HOME/.oh-my-zsh/custom/plugins"
+RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+mkdir -p "$HOME/.oh-my-zsh/custom/plugins"
 mkdir "$HOME/.oh-my-zsh/custom/themes"
 
 # Install improved VI Mode ZSH plugin.
-git clone https://github.com/jeffreytse/zsh-vi-mode \
-  $ZSH_CUSTOM/plugins/zsh-vi-mode
+git clone https://github.com/jeffreytse/zsh-vi-mode ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-vi-mode
 
 # Install powerlevel10k ZSH theme.
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
