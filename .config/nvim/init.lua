f8d196c7a0f35d4c246c0be3fabf19cd856118c5
Alex Ford <Alex@CatDadCode.dev>
2023-09-24 15:46:42 -0600

    Add hack for clangd warning spam. Neovim must fix upstream.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Hack to eliminate constant offset encoding warnings in insert mode when clangd is attached.
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.offsetEncoding = { "utf-16" }
+require("lspconfig").clangd.setup({ capabilities = capabilities })
